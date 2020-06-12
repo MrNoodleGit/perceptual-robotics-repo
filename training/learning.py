f4ae11d66eb2e85bf4ed8ff6c44621f44005dc31
Ronald Alvarez <ronald1andres2@gmail.com>
2020-06-12 11:45:13 -0400

    add build and training files

---
+++ training/learning.py
@@ -0,0 +1,15 @@
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.base_env import *
+
+env = UnityEnvironment(file_name=None, seed=1)
+
+for i in range(5):
+    behaviors = env.get_behavior_names()
+    print(behaviors)
+    try:
+        print(env.get_behavior_spec(behaviors[0]))
+        print(env.get_steps(behaviors[0])[0][0].obs)
+    except: pass
+    env.step()
+
+env.close()
