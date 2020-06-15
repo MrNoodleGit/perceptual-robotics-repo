3eaf0c1cad993eba2223b5fb61a3306e6ad8fd05
Ronald Alvarez <ronald1andres2@gmail.com>
2020-06-15 15:21:59 -0400

    added filename to test Python script with visual pyramids executable

--- training/learning.py
+++ training/learning.py
@@ -1,9 +1,9 @@
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.base_env import *
 
-env = UnityEnvironment(file_name=None, seed=1)
+env = UnityEnvironment(file_name=../build/UnityEnvironment, seed=1)
 
-for i in range(5):
+for i in range(15):
     behaviors = env.get_behavior_names()
     print(behaviors)
     try:
